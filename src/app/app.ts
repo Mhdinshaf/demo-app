a9da0118d3e41b88e8f0b787a21490117319231b
Mohomed Inshaf <164790727+Mhdinshaf@users.noreply.github.com>
2026-02-10 12:09:55 +0530

    angular practice

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from "./page/nav/nav";
+import { Footer } from "./page/footer/footer";
+import { Home } from "./page/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Nav, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
